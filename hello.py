76cb43bdac0efce7b13e9d731d379a50c64ee043
fardanylabib <fardanylabib@gmail.com>
2026-09-07 11:18:47 +0700

    Menambahkan Kelas dan Kampus

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("A. Labib Fardany") #nama sendiri
+print("NRP: 5025211211") #NRP sendiri
+print("Prodi: Teknologi Rekayasa Otomasi")
+print("Kampus: Politeknik Negeri Madura")
+print("Nagara: Indonesia")
